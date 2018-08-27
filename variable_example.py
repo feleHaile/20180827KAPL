2120f356349540db2595e6f4d01381dd24af62bf
jstrickler <jstrickler@gmail.com>
2018-08-27 12:57:40 -0400

    variables

---
+++ variable_example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+x = 5
+y = 10
+thing = 'Wankel rotary engine'
+
+result = x + y
+print(result)
+
+# PEMDAS
+
+print("wankel rotary engine", result, "engine")
+print(f"wankel rotary engine {result} engine")
+print("wankel rotary engine {} engine".format(result))
+
+result = thing + x
+
+print("result is", result)
+
